db3f79362174e6b9cb899918e7b5dc2b356fadb8
Евгений Кудинов <kudinov@upt24.ru>
2017-08-23 17:04:06 +0300

    v0.4

--- bg-click.js
+++ bg-click.js
@@ -1,29 +1,3 @@
-var delay = 1;
-
-function rushToBuy() {
-    var timeOut2 = setTimeout(function () {
-        if (!document.getElementsByClassName('modal_scroller').length) {
-            clearTimeout(timeOut2);
-            console.log('rushToBuy');
-            document.getElementsByClassName('buynow')[0].click();
-            modalClick();
-        } else {
-            rushToBuy();
-        }
-    }, delay);
-}
-
-function modalClick() {
-    var timeOut = setTimeout(function () {
-        if (document.getElementsByClassName('modal_scroller').length) {
-            clearTimeout(timeOut);
-            document.getElementsByClassName('modal_msgbox_button')[0].click();
-            rushToBuy();
-        } else {
-            modalClick();
-        }
-    }, delay)
-}
-
-document.getElementsByClassName('buynow')[0].click();
-modalClick();
+setInterval(function () {
+    document.getElementsByClassName('modal_msgbox_button')[0].click();
+}, 50);
